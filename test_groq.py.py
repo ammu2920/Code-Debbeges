9c814a315c80c6bfab8b8b37f55152c12032dc83
ammu2920 <ammu05300@gmail.com>
2026-03-07 14:42:53 +0530

    Add files via upload

---
+++ test_groq.py.py
@@ -0,0 +1,16 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {"role": "user", "content": "Explain Neural Networks"}
+    ]
+)
+
+print(response.choices[0].message.content)
